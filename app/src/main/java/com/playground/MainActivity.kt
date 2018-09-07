d981c6effce1d5d9eeb08104dd079120b36c911f
Emil Ivanov <emil.ivanov@flatrocktech.com>
2018-09-07 06:16:09 +0300

    fix: Gradle configuration support JAVA 8

--- app/src/main/java/com/playground/MainActivity.kt
+++ app/src/main/java/com/playground/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.playground
 
+import android.arch.lifecycle.LiveData
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.playground.database.AppDatabase
+import com.playground.database.NoteEntry
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mDb = AppDatabase.getInstance(this)
+        mDb.noteDao().insertNote(NoteEntry())
+
+
+        // TODO 1. Make the insert queries on new thread.
+        // TODO 2. List the Notes
+    }
+
+
+    fun retrieveNotes() {
+        var notesList: LiveData<NoteEntry>
     }
 }
--- app/src/main/java/com/playground/database/NoteDao.java
+++ app/src/main/java/com/playground/database/NoteDao.java
@@ -1,13 +1,12 @@
 package com.playground.database;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
-
-import java.util.List;
 
 /**
  * Created by emil.ivanov on 9/2/18.
@@ -16,7 +15,7 @@ import java.util.List;
 public interface NoteDao {
 
     @Query("SELECT * FROM note ORDER BY priority")
-    List<NoteEntry> loadAllNotes();
+    LiveData<NoteEntry> loadAllNotes();
 
     @Insert
     void insertNote(NoteEntry noteEntry);
